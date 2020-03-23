bfdb0d1eef60c1185e22b1f40774db27e0c41ee6
Aidan Shea <aidanshea1@gmail.com>
2020-03-22 19:08:44 -0500

    With .gitignore

---
+++ WeatherGift/APIkeys.swift
@@ -0,0 +1,13 @@
+//
+//  APIkeys.swift
+//  WeatherGift
+//
+//  Created by Aidan Shea on 3/22/20.
+//  Copyright © 2020 Aidan Shea. All rights reserved.
+//
+
+import Foundation
+
+struct APIkeys {
+    static let googlePlacesKey = "AIzaSyBVHe22h5o1Zxkstj_zQLYazrpeZXJaj7M"
+}
