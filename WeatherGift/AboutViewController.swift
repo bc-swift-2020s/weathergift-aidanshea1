a16bce00964d7c81e3880a0d80a82c7d97e05794
Aidan Shea <aidanshea1@gmail.com>
2020-04-20 16:48:01 -0500

    Completed 7.22

---
+++ WeatherGift/AboutViewController.swift
@@ -0,0 +1,21 @@
+//
+//  AboutViewController.swift
+//  WeatherGift
+//
+//  Created by Aidan Shea on 4/20/20.
+//  Copyright © 2020 Aidan Shea. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func doneButtonPressed(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+    }
+}
